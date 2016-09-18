7173e28f187152c42d4fccab72b119d9c991ea1f
Ryan Whitmire <ryanw51@gmail.com>
2016-09-18 09:56:31 -0400

    reorganize for npm

--- index.js
+++
@@ -1,7 +0,0 @@
-import Router from './lib/Router'
-import navigate from './lib/navigate'
-
-export {
-  Router,
-  navigate
-}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Router from './Router'
+import navigate from './navigate'
+
+export {
+  Router,
+  navigate
+}
